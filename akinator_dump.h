e10da9f6f270cb74d380c95aee5e6417c6a28d23
Vostrikova Kira <kirakaramisheva2007@gmail.com>
2026-05-02 22:47:36 +0300

    Delete akinator_dump.h

--- akinator_dump.h
+++
@@ -1,6 +0,0 @@
-#include <cstdio>
-#include "types.h"
-
-void DumpTree(const node_t* node, FILE* filee);
-void DumpGraphNode(const node_t* node, FILE* filee);
-void MakeCommunicationBetweenNodes(const node_t* node, FILE* filee);
